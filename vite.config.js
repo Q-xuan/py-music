cf262462eade97abc7aebda808341a58b2cc343c
黄小迪 <2458579424@qq.com>
2023-05-24 18:11:59 +0800

    端口号5173

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    host:'127.0.0.1' ,//ip地址
+    port: 5173, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+  }
+
 })
